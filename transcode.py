43f136196aea327bc1418a30cc1dd0b540ba5c33
Raphael Jorge <raphaeljsn1@gmail.com>
2024-10-08 22:04:32 -0300

    remove last bracket call

--- transcode.py
+++ transcode.py
@@ -333,7 +333,7 @@ def get_transcode_dir(flac_dir, output_dir, output_format, resample):
     full_flac_dir = flac_dir
     transcode_dir = os.path.basename(flac_dir)
     transcode_dir = extract_first_value(transcode_dir)
-    transcode_dir = remove_last_bracket(transcode_dir)
+    # transcode_dir = remove_last_bracket(transcode_dir)
     flac_dir = transcode_dir
 
     # This is what happens when you spend your time transcoding 24 bit to 16 for
